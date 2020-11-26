cfe31a438f0a11eb3f0ef3136e6c1ba012125bc6
vladlub93 <55275832+vladlub93@users.noreply.github.com>
2020-11-26 12:21:11 +0200

    commit application

---
+++ src/components/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { ContactsProvider } from '../context/ContactsProvider';
+import { ConversationsProvider } from '../context/ConversationsProvider';
+import { SocketProvider } from '../context/SocketProvider'
+import useLocalStorage from '../hooks/useLocalStorage'
+import Dashboard from './Dashboard'
+import Login from './Login';
+
+
+function App() {
+
+    const [id, setId] = useLocalStorage('id')
+
+    const dashboard = (
+        <SocketProvider id={id}>
+        <ContactsProvider>
+            <ConversationsProvider id={id}>
+                <Dashboard id={id} />
+            </ConversationsProvider>
+        </ContactsProvider>
+        </SocketProvider>
+    )
+
+    return (id ? dashboard : <Login onIdSubmit={setId}/>
+
+    );
+}
+
+export default App;
